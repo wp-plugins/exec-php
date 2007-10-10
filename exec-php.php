216a2686fe48f4d798a03f824f4e9e115c46b371
McShelby <McShelby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-10 05:34:46 +0000

    - For WordPress 2.0 execution of PHP is now restricted to Administrators or Editors - Supporting alternative PHP tags [?php ?]
    git-svn-id: https://plugins.svn.wordpress.org/exec-php/trunk@21294 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- exec-php.php
+++ exec-php.php
@@ -1,54 +1,110 @@
 <?php
 /*
-Plugin Name: Exec PHP
-Plugin URI: http://www.soeren-weber.net/post/2005/08/18/50
-Description: Allows &lt;?PHP ?&gt; tags inside of your posts.
-Version: 1.2
+Plugin Name: Exec-PHP
+Plugin URI: http://www.soeren-weber.net/post/2005/08/18/50/
+Description: Allows &lt;?PHP ?&gt; or [?php ?] tags inside of your posts to execute PHP code. For WP 2.0 you can restrict PHP execution by assigning the role exec_php which is assigned by default to the administrators and editors role.
+Version: 2.0
 Author: S&ouml;ren Weber
 Author URI: http://soeren-weber.net
 */
 
-function execphp_replace($match)
+/* Remarks:
+In WP 2.0 post authors can do the following related to their assigned capabilities:
+
+cap                 write/edit PHP with   write/edit PHP with   execute PHP
+                    [?php ?] tags         <?php ?> tags
+=================   ===================   ===================   ===========
+exec_php                                                             X
+unfiltered_html             X                      X
+<none from above>           X
+*/
+
+define('EXECPHP_VERSION', '2.0');
+define('EXECPHP_CAP', 'exec_php');
+
+function execphp_fix_tag($match)
 {
-  // replacing WPs strange PHP tag handling with a functioning tag pair
-  $output = '<?php'. $match[2]. '?>';
-  return $output;
+	// replacing WPs strange PHP tag handling with a functioning tag pair
+	$output = '<?php'. $match[2]. '?>';
+	return $output;
 }
 
-function execphp_apply($content)
+function execphp_eval_php($content)
 {
-  // for debugging also group unimportant components with ()
-  // to check them with a print_r($matches)
-  $pattern = '/'.
-    '(<[\s]*\?php)'. // the opening of the <?php tag
-    '(((([\'\"])([^\\\5]|\\.)*?\5)|(.*?))*)'. // ignore content of PHP quoted strings
-    '(\?>)'. // the closing ? > tag
-    '/is';
-  $content = preg_replace_callback($pattern, 'execphp_replace', $content);
+	// for debugging also group unimportant components with ()
+	// to check them with a print_r($matches)
+	$pattern = '/'.
+		'(?:(?:<)|(\[))[\s]*\?php'. // the opening of the <?php or [?php tag
+		'(((([\'\"])([^\\\5]|\\.)*?\5)|(.*?))*)'. // ignore content of PHP quoted strings
+		'\?(?(1)\]|>)'. // the closing ? > or ?] tag
+		'/is';
+	$content = preg_replace_callback($pattern, 'execphp_fix_tag', $content);
+	// to be compatible with older PHP4 installations
+	// don't use fancy ob_XXX shortcut functions
+	ob_start();
+	eval(" ?> $content <?php ");
+	$output = ob_get_contents();
+	ob_end_clean();
+	return $output;
+}
 
-  // to be compatible with older PHP4 installations
-  // don't use fancy ob_XXX shortcut functions
-  ob_start();
-  eval(" ?> $content <?php ");
-  $output = ob_get_contents();
-  ob_end_clean();
-  return $output;
+function execphp_restricted_eval_php($content)
+{
+	global $post;
+	if (!isset($post) || !isset($post->post_author))
+		return $content;
+	$poster = new WP_User($post->post_author);
+	if (!$poster->has_cap(EXECPHP_CAP))
+		return $content;
+	return execphp_eval_php($content);
+}
+
+// --------------------------------------------------------------------------
+// activating and installing the plugin
+// --------------------------------------------------------------------------
+
+function execphp_install_cap()
+{
+	global $wp_roles;
+
+	// if there is at least one role with the EXECPHP_CAP capability, then
+	// the plugin was previously been installed and we must not do
+	// anything; don't rely that the cap is attachted to the same roles
+	// as during installation because this could already be changed
+	// by the administrator
+
+	foreach($wp_roles->role_objects as $role)
+	{
+		if ($role->has_cap(EXECPHP_CAP))
+			return;
+	}
+
+	$role = get_role('administrator');
+	if ($role !== NULL)
+		$role->add_cap(EXECPHP_CAP);
+	$role = get_role('editor');
+	if ($role !== NULL)
+		$role->add_cap(EXECPHP_CAP);
+}
+
+function execphp_activate_1_x()
+{
+	add_filter('the_content', 'execphp_eval_php', 1);
+}
+
+function execphp_activate()
+{
+	add_action('admin_menu', 'execphp_install_cap');
+	add_filter('the_content', 'execphp_restricted_eval_php', 1);
 }
 
 function execphp_init()
 {
-  add_filter('the_content', 'execphp_apply', 1);
+	global $wp_version;
+	if (substr($wp_version, 0, 2) == "1.")
+		execphp_activate_1_x();
+	else
+		execphp_activate();
 }
-
-if (function_exists('get_currentuserinfo'))
-{
-  // WP 1.5, get_currentuserinfo already loaded
-  execphp_init();
-}
-else
-{
-  // Need to wait until pluggable functions have been loaded
-  // e.g. the Gravatars plugin
-  add_action('init', 'execphp_init');
-}
+add_filter('init', 'execphp_init');
 ?>
