454dc4145ce82c591a8160a733053ecedf030cde
McShelby <McShelby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-10 05:29:10 +0000

    Version 1.0 history checkin
    git-svn-id: https://plugins.svn.wordpress.org/exec-php/trunk@21288 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ exec-php.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Exec PHP
+Plugin URI: http://www.soeren-weber.net/post/2005/08/18/50
+Description: Allows &lt;?PHP ?&gt; tags inside of your posts.
+Version: 1.0
+Author: S&ouml;ren Weber
+Author URI: http://soeren-weber.net
+*/
+
+function execphp_replace($match)
+{
+  if (empty($match['2']))
+    return "";
+
+  // to be compatible with older PHP4 installations
+  // don't use fancy ob_XXX shortcut functions
+  ob_start();
+  eval($match['2']);
+  $output = ob_get_contents();
+  ob_end_clean();
+  return $output;
+}
+
+function execphp_apply($content)
+{
+  // for debugging also group unimportant components with ()
+  // to check them with a print_r($matches)
+  $pattern = '/'.
+    '(<[\s]*\?php)'. // the opening of the <?php tag
+    '(((\'(.*?)\')|(\"(.*?)\")|(.*?))*?)'. // ignore content of PHP quoted strings
+    '(\?>)'. // the closing ? > tag
+    '/is';
+
+  return preg_replace_callback($pattern, 'execphp_replace', $content);
+}
+
+function execphp_init()
+{
+  add_filter('the_content', 'execphp_apply', 3);
+}
+
+if (function_exists('get_currentuserinfo'))
+{
+  // WP 1.5, get_currentuserinfo already loaded
+  execphp_init();
+}
+else
+{
+  // Need to wait until pluggable functions have been loaded
+  // e.g. the Gravatars plugin
+  add_action('init', 'execphp_init');
+}
+?>
