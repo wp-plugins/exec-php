eef8de5f5c3e241dc2d063013d60f8de3bb7f099
McShelby <McShelby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-06 23:28:39 +0000

    Fixing typo
    git-svn-id: https://plugins.svn.wordpress.org/exec-php/trunk@84718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 function ExecPhp_setMessage(heading, text)
 {
 	var message = '<p><strong>' + heading + '</strong> ' + text + '</p>';
-	var parent = document.getElementById("execphp-message");
+	var parent = document.getElementById(execphpCommonL10n.messageContainer);
 	try
 	{
 		container = document.createElement("div");
