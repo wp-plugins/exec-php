58fc8dfdfd5613a8b1a2fb206588103b0413e580
McShelby <McShelby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-06 12:42:53 +0000

    - Allowing for plugin downgrade - Avoid defines if class already exists
    git-svn-id: https://plugins.svn.wordpress.org/exec-php/trunk@84545 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/option.php
+++ includes/option.php
@@ -2,17 +2,18 @@
 
 require_once(dirname(__FILE__).'/const.php');
 
-define('ExecPhp_OPTION_VERSION', 'version');
-define('ExecPhp_OPTION_WIDGET_SUPPORT', 'widget_support');
-define('ExecPhp_OPTION_HAS_OLD_STYLE', 'exec-php_has_old_style');
-define('ExecPhp_OPTION_IGNORE_OLD_STYLE_WARNING', 'exec-php_ignore_old_style_warning');
-
 // -----------------------------------------------------------------------------
 // the ExecPhp_Option class handles the loading and storing of the
 // plugin options including all needed conversion routines during upgrade
 // -----------------------------------------------------------------------------
 
 if (!class_exists('ExecPhp_Option')) :
+
+define('ExecPhp_OPTION_VERSION', 'version');
+define('ExecPhp_OPTION_WIDGET_SUPPORT', 'widget_support');
+define('ExecPhp_OPTION_HAS_OLD_STYLE', 'exec-php_has_old_style');
+define('ExecPhp_OPTION_IGNORE_OLD_STYLE_WARNING', 'exec-php_ignore_old_style_warning');
+
 class ExecPhp_Option
 {
 	var $m_status = ExecPhp_STATUS_UNINITIALIZED;
@@ -41,51 +42,35 @@ class ExecPhp_Option
 		while ($old_version != ExecPhp_VERSION)
 		{
 			$this->load();
-			if ($old_version == '0')
+			if (version_compare($old_version, '4.0') < 0)
 			{
-				// this is first installation of the plugin or upgrade from a version
-				// prior to 4.0;
-				// still needed for deletion from the database - these are obsolete
-				// since version 3.1
-				delete_option(ExecPhp_OPTION_HAS_OLD_STYLE);
-				delete_option(ExecPhp_OPTION_IGNORE_OLD_STYLE_WARNING);
-
-				// be sure standard roles are available, these may be deleted or
-				// renamed by the blog administrator
-				$role = get_role('administrator');
-				if ($role !== NULL)
-					$role->add_cap(ExecPhp_CAPABILITY_EXECUTE_ARTICLES);
+				$this->upgrade_to_4_0();
 				$old_version = '4.0';
 			}
-			else if ($old_version == '4.0')
+			else if (version_compare($old_version, '4.1') < 0)
 				$old_version = '4.1';
-			else if ($old_version == '4.1')
+			else if (version_compare($old_version, '4.2') < 0)
 			{
-				// be sure standard roles are available, these may be deleted or
-				// renamed by the blog administrator
-				$role = get_role('administrator');
-				if ($role !== NULL)
-					$role->add_cap(ExecPhp_CAPABILITY_EDIT_OTHERS_PHP);
+				$this->upgrade_to_4_2();
 				$old_version = '4.2';
 			}
-			else if ($old_version == '4.2')
+			else if (version_compare($old_version, '4.3') < 0)
 				$old_version = '4.3';
-			else if ($old_version == '4.3')
+			else if (version_compare($old_version, '4.4') < 0)
 				$old_version = '4.4';
-			else if ($old_version == '4.4')
+			else if (version_compare($old_version, '4.5') < 0)
 				$old_version = '4.5';
-			else if ($old_version == '4.5')
+			else if (version_compare($old_version, '4.6') < 0)
 				$old_version = '4.6';
-			else if ($old_version == '4.6')
+			else if (version_compare($old_version, '4.7') < 0)
 				$old_version = '4.7';
-			else if ($old_version == '4.7')
+			else if (version_compare($old_version, '4.8') < 0)
 				$old_version = '4.8';
 			else
-			{
-				// very bad; programming error or system is messed up
-				$this->m_version = $old_version;
-				return ExecPhp_STATUS_PLUGIN_VERSION_MISMATCH;
-			}
+				// we are downgrading to an older version of the plugin by
+				// resetting the version to 0 and walking up the conversion path
+				$old_version = '0';
+
 			$this->m_version = $old_version;
 			$this->save();
 		}
@@ -127,6 +112,31 @@ class ExecPhp_Option
 		else
 			$version = $option[ExecPhp_OPTION_VERSION];
 		return $version;
+	}
+
+	function upgrade_to_4_0()
+	{
+		// this is first installation of the plugin or upgrade from a version
+		// prior to 4.0;
+		// still needed for deletion from the database - these are obsolete
+		// since version 3.1
+		delete_option(ExecPhp_OPTION_HAS_OLD_STYLE);
+		delete_option(ExecPhp_OPTION_IGNORE_OLD_STYLE_WARNING);
+
+		// be sure standard roles are available, these may be deleted or
+		// renamed by the blog administrator
+		$role = get_role('administrator');
+		if ($role !== NULL)
+			$role->add_cap(ExecPhp_CAPABILITY_EXECUTE_ARTICLES);
+	}
+
+	function upgrade_to_4_2()
+	{
+		// be sure standard roles are available, these may be deleted or
+		// renamed by the blog administrator
+		$role = get_role('administrator');
+		if ($role !== NULL)
+			$role->add_cap(ExecPhp_CAPABILITY_EDIT_OTHERS_PHP);
 	}
 
 	// ---------------------------------------------------------------------------
