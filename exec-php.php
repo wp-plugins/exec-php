38a43aaf1145c2b0d6adec264109b136d983806a
McShelby <McShelby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-14 13:45:20 +0000

    Bumping version to 4.10.dev
    git-svn-id: https://plugins.svn.wordpress.org/exec-php/trunk@240960 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- exec-php.php
+++ exec-php.php
@@ -5,7 +5,7 @@ Plugin URI: http://bluesome.net/post/2005/08/18/50/
 Description: Executes &lt;?php ?&gt; code in your posts, pages and text widgets.
 Author: S&ouml;ren Weber
 Author URI: http://bluesome.net
-Version: 4.9
+Version: 4.10.dev
 */
 
 require_once(dirname(__FILE__).'/includes/manager.php');
@@ -15,10 +15,11 @@ require_once(dirname(__FILE__).'/includes/manager.php');
 // ----------------------------------------------------------------------------
 
 global $g_execphp_manager;
-if (!isset($g_execphp_manager))
+if (!isset($g_execphp_manager)) {
 	// strange assignment because of explaination how references work;
 	// this will generate warnings with error_reporting(E_STRICT) using PHP5;
 	// http://www.php.net/manual/en/language.references.whatdo.php
 	$GLOBALS['g_execphp_manager'] =& new ExecPhp_Manager();
+}
 
 ?>
